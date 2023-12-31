ef6cca5f9d3354e19a8087c2d1c752a8c08fcef0
kiart-tantasi <petch_rwb@hotmail.com>
2023-12-31 16:32:23 +0700

    lint files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App" data-testid='root-app'>
+    <div className="App" data-testid="root-app">
       Root
     </div>
   );
