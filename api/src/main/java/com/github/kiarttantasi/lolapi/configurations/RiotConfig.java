fd06a2d892a46bd3f90d6c648966e11c60e39522
kiart-tantasi <petch_rwb@hotmail.com>
2024-03-09 16:08:56 +0700

    use only 1 api key

--- api/src/main/java/com/github/kiarttantasi/lolapi/configurations/RiotConfig.java
+++ api/src/main/java/com/github/kiarttantasi/lolapi/configurations/RiotConfig.java
@@ -1,33 +1,18 @@
 package com.github.kiarttantasi.lolapi.configurations;
 
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
-@Slf4j
 public class RiotConfig {
   @Value("${riot.api.key}")
   private String riotApiKey;
-  // TEMP USE, we don't have production key so we hit rate limit so often. let's use two 2 dev keys at the same time
-  @Value("${riot.api.key.secondary:#{null}}")
-  private String riotApiKeySecondary;
   @Value("${riot.api.region.account}")
   private String regionAccount;
   @Value("${riot.api.region.match}")
   private String regionMatch;
   @Value("#{new Integer('${riot.api.match.amount}')}")
   private Integer matchAmount;
-
-  // simple load-balancer to select riot api key
-  public String getRiotApiKey() {
-    if (this.riotApiKeySecondary != null && Math.random() > 0.5) {
-      log.info("using secondary riot api key");
-      return this.riotApiKeySecondary;
-    }
-    log.info("using primary riot api key");
-    return this.riotApiKey;
-  }
 }
