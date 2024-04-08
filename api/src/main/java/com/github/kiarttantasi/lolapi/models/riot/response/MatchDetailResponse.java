817d7341ce4ae9806f817a867fb2f3bc8cb4d43f
kiart-tantasi <petch_rwb@hotmail.com>
2024-04-08 12:16:45 +0700

    suppress another Read of unwritten field

--- api/src/main/java/com/github/kiarttantasi/lolapi/models/riot/response/MatchDetailResponse.java
+++ api/src/main/java/com/github/kiarttantasi/lolapi/models/riot/response/MatchDetailResponse.java
@@ -1,16 +1,14 @@
 package com.github.kiarttantasi.lolapi.models.riot.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MatchDetailResponse {
   private MatchInfo info;
 
+  @SuppressFBWarnings // to suppress "M C NP: Read of unwritten field"
   public MatchInfo getInfo() {
     return this.info.deepClone();
-  }
-
-  public void setInfo(MatchInfo info) {
-    this.info = info.deepClone();
   }
 }
