bc2597e2d34a2d7a57a5a27f30744108c09a30bd
kiart-tantasi <petch_rwb@hotmail.com>
2024-04-29 10:24:20 +0700

    handle env files

--- go-api/main.go
+++ go-api/main.go
@@ -5,15 +5,32 @@ import (
 	"go-api/controllers"
 	v1 "go-api/controllers/v1"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// env
+	env := os.Getenv("ENV")
+	if env == "production" {
+		godotenv.Load(".env.production")
+	} else {
+		env = "development"
+		godotenv.Load(".env")
+	}
+	fmt.Printf("running with profile \"%s\"\n", env)
+
+	// routing
 	healthController := &controllers.HealthController{}
 	matchController := &v1.MatchController{}
 	http.HandleFunc("/api/health", healthController.GetHealth)
 	http.HandleFunc("/api/matches", matchController.GetMatches)
-	fmt.Println("app is listening and serving")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+	// start
+	port := os.Getenv("SERVER_PORT")
+	fmt.Println("app is listening and serving on port", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		panic(err)
 	}
 }
