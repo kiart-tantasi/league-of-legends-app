0a79e0f8a665ac2d7fc902543b9405e17697871c
kiart-tantasi <petch_rwb@hotmail.com>
2024-04-29 21:45:31 +0700

    draft func getPuuid

--- go-api/services/match_service.go
+++ go-api/services/match_service.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +18,16 @@ func (matchService *MatchService) GetMatches(gameName, tagLine string) string {
 }
 
 func getPuuid(gameName, tagLine string) string {
+	// TODO: handle config
+	var riotConfig *RiotConfig
+	url := fmt.Sprintf("https://%s.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s", riotConfig.getRegionAccount(), gameName, tagLine)
+	// TODO: add header "X-Riot-Token"
+	if res, err := http.Get(url); err != nil {
+		panic(err)
+	} else {
+		fmt.Println(res.Body)
+		// TODO: parse json
+	}
 	return ""
 }
 
@@ -27,4 +40,24 @@ func getMatches(matchIds []string) []string {
 		fmt.Println("matchId:", matchId)
 	}
 	return matchIds
+}
+
+// riot config
+type RiotConfig struct{}
+
+func (*RiotConfig) getRiotApiKey() string {
+	return os.Getenv("RIOT_API_KEY")
+}
+func (*RiotConfig) getRegionAccount() string {
+	return os.Getenv("RIOT_API_REGION_ACCOUNT")
+}
+func (*RiotConfig) getRegionMatch() string {
+	return os.Getenv("RIOT_API_REGION_MATCH")
+}
+func (*RiotConfig) getMatchAmount() int {
+	if matchAmount, err := strconv.Atoi(os.Getenv("RIOT_MATCH_AMOUNT")); err != nil {
+		panic("RIOT_MATCH_AMOUNT is not set properly")
+	} else {
+		return matchAmount
+	}
 }
