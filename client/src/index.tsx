748f6320c4dd3e2c04fb412cc94b3e91e66de410
kiart-tantasi <petch_rwb@hotmail.com>
2024-04-30 00:51:23 +0700

    test client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -29,3 +29,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+// foobar
