e1898f32042a2f1219f24a01cef86418d17d37c8
Kiart Tantasi <76615399+kiart-tantasi@users.noreply.github.com>
2024-05-04 19:02:39 +0700

    fix: Function to send json response (#22)

--- go-api/internal/health/handler.go
+++ go-api/internal/health/handler.go
@@ -2,10 +2,13 @@ package health
 
 import (
 	"net/http"
+	"time"
 )
 
 type HealthHandler struct{}
 
 func (*HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// fake server time
+	time.Sleep(500 * time.Millisecond)
 	w.WriteHeader(http.StatusOK)
 }
