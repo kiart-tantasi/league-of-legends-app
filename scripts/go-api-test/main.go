aad416fa9e54f4999b1aecfb3b61b5a222a1cfe9
Kiart Tantasi <76615399+kiart-tantasi@users.noreply.github.com>
2024-05-07 10:14:09 +0700

    feat: Reduce complexity of script (#31)
    * reduce complexity of script
    
    * rename job

--- scripts/go-api-test/main.go
+++ scripts/go-api-test/main.go
@@ -19,36 +19,28 @@ func main() {
 func checkHealthEndpoint(client *http.Client) {
 	isSuccess := false
 	for i := 0; i < 5; i++ {
-		fmt.Println("round", i+1)
-		isSuccessChannel := make(chan bool)
-		go func() {
-			res, err := client.Get("http://localhost:8080/api/health")
-			if err == nil && res.StatusCode == 200 {
-				isSuccessChannel <- true
-			} else {
-				isSuccessChannel <- false
-			}
-		}()
-		isSuccess = <-isSuccessChannel
-		if isSuccess {
+		fmt.Println("health endpoint round", i+1)
+		res, err := client.Get("http://localhost:8080/api/health")
+		if err == nil && res.StatusCode == 200 {
+			isSuccess = true
 			break
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 	if !isSuccess {
-		panic("health failed")
+		panic("health endpoint failed")
 	}
 }
 
 func checkMatchesEndpoint(client *http.Client) {
 	res, err := client.Get("http://localhost:8080/api/v1/matches?gameName=GAMENAME&tagLine=TAGLINE")
 	if err != nil || res.StatusCode != 200 || res.Body == nil {
-		panic("matches failed")
+		panic("matches endpoint failed")
 	}
 	defer res.Body.Close()
 	bytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic("matches failed")
+		panic("matches endpoint failed")
 	}
 	fmt.Println(string(bytes))
 }
