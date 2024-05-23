72cd124729772ad61683755eabaf089c08f3e313
kiart-tantasi <petch_rwb@hotmail.com>
2024-05-23 23:33:58 +0700

    remove context.TODO()

--- go-api/internal/cache/mongoclient.go
+++ go-api/internal/cache/mongoclient.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -16,14 +17,18 @@ var databaseName string
 func InitMongoClient(uri string) {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-	c, err := mongo.Connect(context.TODO(), opts)
+	ctx1, cancel1 := context.WithTimeout(context.Background(), 5000*time.Millisecond)
+	defer cancel1()
+	c, err := mongo.Connect(ctx1, opts)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	client = c
 	databaseName = os.Getenv("CACHE_MONGODB_DATABASE_NAME")
-	err = client.Database(databaseName).RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err()
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	defer cancel2()
+	err = client.Database(databaseName).RunCommand(ctx2, bson.D{{"ping", 1}}).Err()
 	if err != nil {
 		log.Println(err)
 	} else {
