9d2d3e676c0a51051aec8f10412898dc88b1fec0
kiart-tantasi <petch_rwb@hotmail.com>
2024-05-26 13:16:58 +0700

    log when status code is not OK

--- api/src/main/java/com/github/kiarttantasi/lolapi/services/ApiService.java
+++ api/src/main/java/com/github/kiarttantasi/lolapi/services/ApiService.java
@@ -6,10 +6,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ApiService {
   private static final HttpResponse.BodyHandler<String> BODYHANDLER =
       HttpResponse.BodyHandlers.ofString();
@@ -18,6 +20,11 @@ public class ApiService {
       throws IOException, InterruptedException {
     final HttpResponse<String> response = HttpClient.newHttpClient().send(request, BODYHANDLER);
     if (response.statusCode() != HttpStatus.OK.value()) {
+      log.warn(
+          String.format("request with URI %s got status code %s and response body %s",
+              response.uri(),
+              response.statusCode(),
+              response.body()));
       return null;
     }
     return new ObjectMapper().readValue(response.body(), mappingClass);
